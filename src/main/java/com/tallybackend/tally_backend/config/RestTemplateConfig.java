2cba577498459f4385f7d65930043924cac699c3
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2026-01-12 12:08:00 +0530

    test_run

--- src/main/java/com/tallybackend/tally_backend/config/RestTemplateConfig.java
+++ src/main/java/com/tallybackend/tally_backend/config/RestTemplateConfig.java
@@ -1,10 +1,14 @@
 package com.tallybackend.tally_backend.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
 
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- src/test/java/com/tallybackend/tally_backend/testTallyClient.java
+++ src/test/java/com/tallybackend/tally_backend/testTallyClient.java
@@ -1,18 +1,52 @@
 package com.tallybackend.tally_backend;
 
 import com.tallybackend.tally_backend.client.TallyClient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @SpringBootTest
-public class testTallyClient {
+class TallyClientTest {
 
     @Autowired
-    private TallyClient client;
+    private TallyClient tallyClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private MockRestServiceServer mockServer;
+
+    @BeforeEach
+    void setup() {
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+    }
 
     @Test
-    public void testClientFase(){
-        String output = client.
+    void shouldFetchCompanyXml() {
+
+        String mockXmlResponse =
+                "<COMPANIES><COMPANY>ABC</COMPANY></COMPANIES>";
+
+        mockServer.expect(requestTo("http://localhost:9000"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withSuccess(
+                        mockXmlResponse,
+                        MediaType.TEXT_XML
+                ));
+
+        String response = tallyClient.fetchCompanyXml();
+
+        assertEquals(mockXmlResponse, response);
+        mockServer.verify();
     }
 }
